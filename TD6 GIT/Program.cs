edfe981ea87b7334e876981d1d8113f21c113163
Pigeon <pigeonevan6@yahoo.com>
2024-04-02 14:14:03 +0200

    Ajout Aire hexagone

--- TD6 GIT/Program.cs
+++ TD6 GIT/Program.cs
@@ -6,13 +6,25 @@ double Aire (double longueur,double Largeur)
     return longueur*Largeur;
 }
 
+double AireHexagone (double longueurCote)
+{
+    return (3 * Math.Sqrt(3) * Math.Pow(longueurCote, 2)) / 2;
+}
 
-double l;
-double L;
+double l, L;
 
 Console.WriteLine("Saisir la longueur du rectangle :");
 L = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Saisir la largeur du rectangle :");
 l = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine($"L'Aire vaut : {Aire(L,l)}");
+double aireRectangle = Aire(L, l);
+
+Console.WriteLine($"L'aire du rectangle est : {aireRectangle}");
+
+
+double longueurCote = 3.8;
+
+double aireHexagone = AireHexagone(longueurCote);
+
+Console.WriteLine($"L'aire de l'hexagone est : {aireHexagone}");
