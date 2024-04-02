3b348eed7f02e673c4182cb781a12ee4c6a0d8f5
Pigeon <pigeonevan6@yahoo.com>
2024-04-02 13:57:30 +0200

    Ajoutez des fichiers projet.

---
+++ TD6 GIT/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+double Aire (double longueur,double Largeur)
+{
+
+    return longueur*Largeur;
+}
+
+
+double l;
+double L;
+
+Console.WriteLine("Saisir la longueur du rectangle :");
+L = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Saisir la largeur du rectangle :");
+l = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"L'Aire vaut : {Aire(L,l)}");
