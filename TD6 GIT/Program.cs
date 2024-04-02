8cc76068deea3a229cd3ab2cbef38403f22a8fe3
Pigeon <pigeonevan6@yahoo.com>
2024-04-02 14:24:45 +0200

    Ajout Documentation sous programme aire rectangle

--- TD6 GIT/Program.cs
+++ TD6 GIT/Program.cs
@@ -1,5 +1,7 @@
 ﻿
 using System;
+//Calcule de l'aire d'un rectangle avec variables longueur et largeur -> passage en parametres
+//Valeur de retour: aire du rectangle
 double Aire (double longueur,double Largeur)
 {
 
@@ -10,9 +12,10 @@ double Aire (double longueur,double Largeur)
 double l;
 double L;
 
+//Saisie des variables 
 Console.WriteLine("Saisir la longueur du rectangle :");
 L = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Saisir la largeur du rectangle :");
 l = Convert.ToDouble(Console.ReadLine());
-
+//Affichage
 Console.WriteLine($"L'Aire vaut : {Aire(L,l)}");
